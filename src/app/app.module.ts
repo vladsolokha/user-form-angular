ffe6108366df616db2886bc3f81e351dab68fa8b
Justin Wheeler <justin.wheeler@gfs.com>
2021-06-30 08:43:14 -0400

    User Components and Initial Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { UserSearchComponent } from './user/user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserFormComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule

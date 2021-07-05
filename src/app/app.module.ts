e8f7c59083206c2fe7c51c521ebaa6e79d1fa678
Justin Wheeler <justin.wheeler@gfs.com>
2021-07-05 14:59:24 -0400

    Angular Material, Routing, Clean Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { UserFormComponent } from './user/user-form/user-form.component';
 import { UserSearchComponent } from './user/user-search/user-search.component';
+import {RouterModule} from "@angular/router";
+import { CardComponent } from './card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatSliderModule} from "@angular/material/slider";
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     UserFormComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    CardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatCardModule,
+    RouterModule.forRoot([
+      { path: 'user', component: UserComponent },
+      { path: '', component: CardComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]

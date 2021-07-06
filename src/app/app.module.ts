ad160d2a218aed651683fb3d7860b273d2ac8ef1
Justin Wheeler <justin.wheeler@gfs.com>
2021-07-06 08:49:41 -0400

    Renamed Card to Home, Created User Form, Added Back Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {UserComponent} from './user/user.component';
 import {UserFormComponent} from './user/user-form/user-form.component';
 import {UserSearchComponent} from './user/user-search/user-search.component';
 import {RouterModule} from "@angular/router";
-import {CardComponent} from './card/card.component';
+import {HomeComponent} from './home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from "./core/core.module";
 import {SharedModule} from "./shared/shared.module";
@@ -15,7 +15,7 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
+    HomeComponent,
     UserComponent,
     UserFormComponent,
     UserSearchComponent,
@@ -29,7 +29,7 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
       {path: 'user', component: UserComponent},
       {path: 'user/form', component: UserFormComponent},
       {path: 'user/search', component: UserSearchComponent},
-      {path: '', component: CardComponent},
+      {path: '', component: HomeComponent},
       {path: '**', redirectTo: '', pathMatch: 'full'},
     ]),
     SharedModule,
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardComponent } from './card.component';
+import { HomeComponent } from './home.component';
 
-describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

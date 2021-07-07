3368c3979768d1bb2d4b8601da5221449c604c0d
Justin Wheeler <justin.wheeler@gfs.com>
2021-07-07 08:36:18 -0400

    Removed User Component, Root Navigates to UserSearch, Added UserForm Navigation from UserSearch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {UserComponent} from './user/user.component';
 import {UserFormComponent} from './user/user-form/user-form.component';
 import {UserSearchComponent} from './user/user-search/user-search.component';
 import {RouterModule} from "@angular/router";
@@ -16,7 +15,6 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent,
     UserFormComponent,
     UserSearchComponent,
   ],
@@ -26,15 +24,15 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
     CoreModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'user', component: UserComponent},
-      {path: 'user/form', component: UserFormComponent},
-      {path: 'user/search', component: UserSearchComponent},
-      {path: '', component: HomeComponent},
-      {path: '**', redirectTo: '', pathMatch: 'full'},
+      {path: 'user', redirectTo: 'user/search', pathMatch: 'full' },
+      {path: 'user/form', component: UserFormComponent },
+      {path: 'user/search', component: UserSearchComponent },
+      {path: '', component: HomeComponent },
+      {path: '**', redirectTo: '', pathMatch: 'full' },
     ]),
     SharedModule,
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
